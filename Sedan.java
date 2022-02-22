1f4fb6028beeffb23bdb9a77b730b319a534f0e1
rashg05 <gajra.rashmi0504@gmail.com>
2022-02-22 20:35:34 +0530

    Added: Sedan class created with getSalePrice method

---
+++ Sedan.java
@@ -0,0 +1,18 @@
+package com.bridgelab.autoshop;
+
+public class Sedan extends Car {
+	
+	int length;
+	
+	public Sedan(int speed, double regularPrice, String color, int length) {
+		super(speed, regularPrice, color);
+		this.length = length;
+	}
+	
+	public double getSalePrice() {
+		if (length > 20)
+			return regularPrice - (regularPrice * 0.05);
+		else
+			return regularPrice - (regularPrice * 0.1);
+	}
+}
